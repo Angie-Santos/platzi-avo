65db17e2afeffcac4834ad0e10b94bfe1c533761
angie <ajsag12@gmail.com>
2023-02-28 11:56:53 -0500

    Add styles to CartProduct component

--- components/CartProduct.tsx
+++ components/CartProduct.tsx
@@ -1,14 +1,15 @@
 import { CartContext } from "context/ContxtProvider";
+import Link from "next/link";
 import React, { useContext } from "react";
 
 const CartProduct = ({ elemProduct }: any) => {
   const { cartProduct, setCartProduct } = useContext<any>(CartContext);
   const handleDeleteButton = () => {
-
     const updateProduct = {
       ...elemProduct,
-      input: parseInt(elemProduct.input) -1
+      input: parseInt(elemProduct.input) - 1,
     };
+
     setCartProduct(
       cartProduct.map((element: any) =>
         element.product.name === elemProduct?.product.name
@@ -20,18 +21,46 @@ const CartProduct = ({ elemProduct }: any) => {
   };
 
   return (
-    <div>
+    <div id="container">
       <img src={elemProduct.product.image} />
-      <div>
-        <p>Name: {elemProduct.product.name}</p>
-        <p>Price: {elemProduct.product.price}</p>
-        <p>und: {elemProduct.input}</p>
+      <div id="text">
+       <Link href={`products/${elemProduct.product.id}`}> <span id="name">{elemProduct.product.name}</span></Link>
+        <p><span>$</span>{elemProduct.product.price}</p>
+        <p><span>Und: </span>{elemProduct.input}</p>
       </div>
       <button onClick={handleDeleteButton}>🗑️</button>
 
       <style jsx>{`
+        #container {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          gap: 1rem;
+        }
         img {
-          width: 10%;
+          width: 30%;
+        }
+        #text {
+          width: 50%;
+          padding-top: 3rem;
+          align-items: flex-start;
+          font-size:1.4rem;
+        }
+
+        #name {
+          font-size: 1.7rem;
+          color:black;
+        }
+
+        span {
+          font-weight:600;
+        }
+
+        button {
+          cursor: pointer;
+          align-items: flex-end;
+          border: none;
+          background-color: transparent;
         }
       `}</style>
     </div>
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -3,31 +3,52 @@ import { CartContext } from "context/ContxtProvider";
 import React, { useContext } from "react";
 
 const Cart = () => {
-  const { cartProduct, setCartProduct } = useContext<any>(CartContext);
+  const { cartProduct } = useContext<any>(CartContext);
   return (
     <>
-      <section>
-        {cartProduct.map((elm: any) => {
-          if(elm.input > 0 ){
-            return <CartProduct key={elm.product.id} elemProduct={elm}/>;
-          }
-        })}
-      </section>
-      {cartProduct.reduce(
-        (prev: any, curr: any) => parseInt(prev) + parseInt(curr.input),
-        0
-      ) === 0 && (
+      <section id="main">
         <section>
-          <p>Your cart is empty</p>
-          <p>
-            You will need to add some items to the cart before you can checkout.
-          </p>
+          <h2>Cart</h2>
+          {cartProduct.map((elm: any) => {
+            if (elm.input > 0) {
+              return <CartProduct key={elm.product.id} elemProduct={elm} />;
+            }
+          })}
+          <br />
         </section>
-      )}
-      <section>
-        <p>Total {0} </p>
-        <button>Check out</button>
+        {cartProduct.reduce(
+          (prev: any, curr: any) => parseInt(prev) + parseInt(curr.input),
+          0
+        ) === 0 && (
+          <section>
+            <p>Your cart is empty</p>
+            <p>
+              You will need to add some items to the cart before you can
+              checkout.
+            </p>
+          </section>
+        )}
+        <section>
+          <p>Total {0} </p>
+          <button>Check out</button>
+        </section>
       </section>
+
+      <style jsx>{`
+        #main{
+          height:100%;
+          width:100%;
+          display:flex;
+          padding-top: 3rem;
+          flex-direction:column;
+          align-items:center;
+        }
+        h2{
+          text-align:center;
+          font-size:2.1rem;
+          color:rgb(50, 96, 58); 
+        }
+      `}</style>
     </>
   );
 };
