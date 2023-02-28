2cb29da17601ee2f55c9aa8ea2fb3229babecb63
angie <ajsag12@gmail.com>
2023-02-28 12:38:26 -0500

    Add responsive styles CartProduct Component

--- components/CartProduct.tsx
+++ components/CartProduct.tsx
@@ -24,18 +24,28 @@ const CartProduct = ({ elemProduct }: any) => {
     <div id="container">
       <img src={elemProduct.product.image} />
       <div id="text">
-       <Link href={`products/${elemProduct.product.id}`}> <span id="name">{elemProduct.product.name}</span></Link>
-        <p><span>$</span>{elemProduct.product.price}</p>
-        <p><span>Und: </span>{elemProduct.input}</p>
+        <Link href={`products/${elemProduct.product.id}`}>
+          {" "}
+          <span id="name">{elemProduct.product.name}</span>
+        </Link>
+        <p>
+          <span>$</span>
+          {elemProduct.product.price}
+        </p>
+        <p>
+          <span>Und: </span>
+          {elemProduct.input}
+        </p>
       </div>
       <button onClick={handleDeleteButton}>🗑️</button>
 
       <style jsx>{`
         #container {
+          gap: 1rem;
+          padding: 1rem;
           width: 100%;
           display: flex;
           justify-content: center;
-          gap: 1rem;
         }
         img {
           width: 30%;
@@ -44,16 +54,16 @@ const CartProduct = ({ elemProduct }: any) => {
           width: 50%;
           padding-top: 3rem;
           align-items: flex-start;
-          font-size:1.4rem;
+          font-size: 1.4rem;
         }
 
         #name {
           font-size: 1.7rem;
-          color:black;
+          color: black;
         }
 
         span {
-          font-weight:600;
+          font-weight: 600;
         }
 
         button {
@@ -61,6 +71,26 @@ const CartProduct = ({ elemProduct }: any) => {
           align-items: flex-end;
           border: none;
           background-color: transparent;
+        }
+
+        @media (max-width: 1000px) {
+          #container {
+            width: 80%;
+            gap: 0;
+            margin-bottom: 2rem;
+            border: 0.1rem lightgray solid;
+            border-radius: 10px;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+          }
+          #text {
+            padding-top: 0;
+            width: 80%;
+          }
+          p{
+            margin:1rem;
+          }
         }
       `}</style>
     </div>
