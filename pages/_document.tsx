7c837248c622c9db7fbd6c195d615b6f2adc687c
angie <ajsag12@gmail.com>
2023-03-03 15:26:48 -0500

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head/>
+      <Head>
+      <title>Platzi Avo</title>
+      <link rel="icon" type="image/x-icon" href="images/avocado-icon.png"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
